5b7eaef4d6516b992f48131dc1920556e5e7f7b5
Donald Cull <20510711+donaldCull@users.noreply.github.com>
2024-08-20 13:05:46 +0100

    show lessons based on date

--- components/employeeSchedule/HeaderListDateSchedule.tsx
+++ components/employeeSchedule/HeaderListDateSchedule.tsx
@@ -1,26 +1,64 @@
-import { StyleSheet, Text, View } from "react-native";
-import Entypo from '@expo/vector-icons/Entypo';
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import DateTimePicker, {
+  DateTimePickerEvent,
+  DateTimePickerAndroid,
+} from "@react-native-community/datetimepicker";
+import React, { useState } from "react";
 
-export default function HeaderListDateSchedule() {
-return (
-  <View style={styles.wrapper}>
-    <Entypo name="chevron-left" size={24} color="#fafafa" />
-    <Text style={styles.dateText}>dd/mm/yyyy</Text>
-    <Entypo name="chevron-right" size={24} color="#fafafa" />
-  </View>
-);
+type HeaderListDateScheduleProps = {
+  currentDate: Date;
+  showCalendar: boolean;
+  setShowCalendar: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function HeaderListDateSchedule({
+  currentDate,
+  showCalendar,
+  setShowCalendar,
+}: HeaderListDateScheduleProps) {
+  return (
+    <View style={styles.wrapper}>
+      <Entypo name="chevron-left" size={24} color="#fafafa" />
+      <Pressable
+        style={styles.calendarButton}
+        onPress={() => setShowCalendar((prevState) => !prevState)}
+      >
+        <Text style={styles.dateText}>{currentDate.toDateString()}</Text>
+        <Entypo name={`chevron-${showCalendar ? "down": "up"}`} size={24} color="#fafafa" />
+      </Pressable>
+      <Entypo name="chevron-right" size={24} color="#fafafa" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
     flexDirection: "row",
+    alignItems: "center",
     justifyContent: "space-between",
     backgroundColor: "#4b90fe",
-    padding: 20
+    padding: 16,
   },
   dateText: {
     fontSize: 18,
     fontWeight: "bold",
-    color: "#fafafa"
-  }
-})
+    color: "#fafafa",
+  },
+  calendarButton: {
+    flexDirection: "row",
+    backgroundColor: "#0a68fe",
+    borderRadius: 10,
+    padding: 10
+  },
+  calendarView: {
+    position: "absolute",
+    backgroundColor: "#fafafa",
+  },
+  shadowProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+});
--- components/employeeSchedule/ScheduleList.tsx
+++ components/employeeSchedule/ScheduleList.tsx
@@ -1,21 +1,84 @@
-import { ClassData, ClassLessons } from "@/types/employeeClass";
-import { FlatList, View } from "react-native";
+import { ClassLessons } from "@/types/employeeClass";
+import { FlatList, StyleSheet, View } from "react-native";
 import EmptyListItem from "../common/EmptyListItem";
 import ScheduleItem from "./ScheduleItem";
 import HeaderListDateSchedule from "./HeaderListDateSchedule";
+import { useEffect, useState } from "react";
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
 
 type ScheduleListProps = {
   schedule: ClassLessons[];
 };
 
 export default function ScheduleList({ schedule }: ScheduleListProps) {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [currentDate, setDate] = useState(new Date("2024-07-08"));
+  const [filteredSchedule, setFilteredSchedule] = useState<ClassLessons[]>();
+
+  const onChange = (_: DateTimePickerEvent, selectedDate?: Date) => {
+    const date = selectedDate;
+    if (date) {
+      setDate(date);
+    }
+    setShowCalendar(false);
+  };
+
+  const isBetweenRange = (lesson: ClassLessons) => {
+    const upperRange = new Date(currentDate);
+    upperRange.setUTCHours(12);
+    return lesson.start_at.getTime() >= currentDate.getTime() && lesson.start_at.getTime() <= upperRange.getTime();
+  }
+
+  useEffect(() => {
+    setFilteredSchedule(schedule.filter(isBetweenRange))
+  }, [currentDate])
+
   return (
-    <FlatList
-      data={schedule}
-      ListEmptyComponent={<EmptyListItem />}
-      ListHeaderComponent={<HeaderListDateSchedule />}
-      renderItem={({ item }) => <ScheduleItem lesson={item} />}
-      keyExtractor={(lesson) => lesson.lessonId}
-    />
+    <>
+      {showCalendar && (
+        <View style={[styles.calendarView, styles.shadowProp]}>
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={currentDate}
+            mode={"date"}
+            onChange={onChange}
+            display="inline"
+          />
+        </View>
+      )}
+
+      <FlatList
+        data={filteredSchedule}
+        ListEmptyComponent={<EmptyListItem />}
+        ListHeaderComponent={
+          <HeaderListDateSchedule
+            showCalendar={showCalendar}
+            setShowCalendar={setShowCalendar}
+            currentDate={currentDate}
+          />
+        }
+        renderItem={({ item }) => <ScheduleItem lesson={item} />}
+        keyExtractor={(lesson) => lesson.lessonId}
+      />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  calendarView: {
+    zIndex: 1,
+    position: "absolute",
+    top: 50,
+    left: 50,
+    backgroundColor: "#fafafa",
+    padding: 5
+  },
+  shadowProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+});
